6db812b5719d98dfa44cf75a7557b0d51877ad3f
Franklin G. Mendoza <frangusdj@hotmail.com>
2019-11-01 07:34:09 -0500

    Conexion terminada

--- src/main/java/com/tecnoshared/estadistica/Logica/conexion.java
+++ src/main/java/com/tecnoshared/estadistica/Logica/conexion.java
@@ -1,14 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.tecnoshared.estadistica.Logica;
 
-/**
- *
- * @author Franklin G. Mendoza
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 public class conexion {
+    private String url,user,clave;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public conexion() {
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
     
+    public Connection conectar(){
+        Connection link = null;
+        try {
+            //Cargamos el driver de conexion
+            Class.forName("org.postgresql.Driver");
+            link = DriverManager.getConnection(getUrl(),getUser(),getClave());
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showConfirmDialog(null, e);
+        }
+        return link;
+    }
 }
